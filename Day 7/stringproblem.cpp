451c1e9c33fe8d8ad08c6dab5664b76538f33cd9
Eshaabhasin <eshaabhasin4@gmail.com>
2023-09-09 22:08:12 +0530

    added code

---
+++ Day 7/stringproblem.cpp
@@ -0,0 +1,15 @@
+//CODECHEF PROBLEM:https://www.codechef.com/problems/LUCKYSEVEN
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char s[10];
+	cin>>s;
+	for(int i=0;i<10;i++){
+	}
+	cout<<s[6];
+	
+	return 0;
+}
