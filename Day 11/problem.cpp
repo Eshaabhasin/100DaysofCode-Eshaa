69ed37f0916dff4090033a015969612004f02570
Eshaabhasin <eshaabhasin4@gmail.com>
2023-09-13 21:56:36 +0530

    added code

---
+++ Day 11/problem.cpp
@@ -0,0 +1,16 @@
+//CODECHEF PROBLEM https://www.codechef.com/START100D/problems/AIRINDEX
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x<100){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
