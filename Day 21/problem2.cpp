f6ac69affeda75c0f2fc6c10aad30f33cf00b0aa
Eshaabhasin <eshaabhasin4@gmail.com>
2023-09-27 22:35:40 +0530

    added code

---
+++ Day 21/problem2.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/START102D/problems/NEWSPAPER?tab=statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    int z,y,n;
+   
+    if(x==10 or x==9 or x==8){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
