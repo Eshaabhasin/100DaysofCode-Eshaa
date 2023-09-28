0bfe2941cd3eb364ec315eff05826f5c1d2f46bd
Eshaabhasin <eshaabhasin4@gmail.com>
2023-09-28 22:56:47 +0530

    added code

---
+++ Day 22/problem.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/practice/PCPP07/problems/CPPCC10
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int N,M;
+	    cin>>N>>M;
+	    cout<<N*M<<endl;
+	    
+	    
+	}
+	return 0;
+}
