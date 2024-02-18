c50c8841d203aa6dbb2136d9d52f862b418f63ed
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-18 21:20:24 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void pairs(){
+   pair<int,int>p={1,3};
+   cout<<p.first<<" "<<p.second<<endl; 
+   pair<int,int>arr[]={{1,2},{2,5},{5,1}};
+   cout<<arr[1].second;
+
+}
+int main(){
+    pairs();
+}
